11c1223bd80fa5807313f5fb9ea08574b85d6190
drmimi47 <drmimi47@gmail.com>
2025-07-09 23:31:00 -0400

    added links

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-//hi
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".label, .value, h1 a");
+  elements.forEach((el) => {
+    const hue = Math.floor(Math.random() * 360);
+    el.style.color = `hsl(${hue}, 100%, 70%)`;
+  });
+});
